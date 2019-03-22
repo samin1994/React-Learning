679158cfaf5754d577d80274bf17fb43598fa9fc
Samin <saminlabbaf1994@gmail.com>
2019-03-22 21:25:38 +0430

    ReConfiguring React

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Navbar, NavbarBrand } from reactstrap;
+import { Navbar, NavbarBrand } from 'reactstrap';
 
 class App extends Component {
   render() {
